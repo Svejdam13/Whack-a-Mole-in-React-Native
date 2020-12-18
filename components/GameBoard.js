e265e3064f57290114d997003fa805b2e21b1546
Martin Svejda <svejdam13@gmail.com>
2020-12-18 20:51:06 +0100

    update

--- components/GameBoard.js
+++ components/GameBoard.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import Square from './Square';
 
 const GameBoard = () => {
-  const [timeLeft, setTimeLeft] = useState(60);
+  const [timeLeft, setTimeLeft] = useState(10);
 
   useEffect(() => {
     if(!timeLeft) return
--- components/Square.js
+++ components/Square.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 const Square = () => {
   const [moleActive, setMoleActive] = useState(false);
+  const [isGameOver, setGameOver] = useState(false);
   
   const randomTime = Math.random() * 20000;
-
+  let timerId;
   useEffect(() => {
-   const timerId = setInterval(() => {
+   timerId = setInterval(() => {
      setMoleActive(true)
      setTimeout(() => {setMoleActive(false)},800)
    }, randomTime)
+   setTimeout(endGame, 10 * 1000)
   }, [])
 
+  function endGame(){
+    clearInterval(timerId);
+    setGameOver(true)
+  }
+
   return (
-    <View style={moleActive ? styles.mole : styles.square}></View>
+    <View style={moleActive ? styles.mole : styles.square}>
+      {isGameOver && <Text>X</Text>}
+    </View>
   )
 }
 
