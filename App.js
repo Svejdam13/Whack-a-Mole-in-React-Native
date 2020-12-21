322a3fe73f6b425e78e6b97d5ff2b6d40bff2dd5
Martin Svejda <svejdam13@gmail.com>
2020-12-21 20:38:05 +0100

    update

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import GameBoard from './components/GameBoard';
+import { Provider } from 'react-redux';
+
 
 export default function App() {
   return (
-    <GameBoard></GameBoard>
+    <Provider>
+      <GameBoard/>
+    </Provider>
   );
 }
 
